456fc1d4f1c2593bb779f67edca2353f5ef2066f
Eugene Lepeico <eugenelepeico@gmail.com>
2022-11-04 11:21:33 +0200

    feat: cli setup

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,53 @@ mod parser;
 use std::io;
 use crate::parser::Parser;
 
-fn main() {
-    // read input from stdin
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+fn get_args() -> std::env::Args {
+    let mut args = std::env::args();
+    args.next();
+    args
+}
 
-    // parse input
-    let mut parser = Parser::new(input);
+fn main() {
+    for arg in get_args() {
+        match arg.as_str() {
+            "-v" | "--version" => {
+                println!("Version: {}", env!("CARGO_PKG_VERSION"));
+                return;
+            }
+            "-h" | "--help" => {
+                println!("Usage: calc [expression]");
+                println!("Options:");
+                println!("  -v, --version  Show version");
+                println!("  -h, --help     Show this help");
+                return;
+            }
+            _ => (),
+        }
+    }
+
+    let mut expression_opt = None;
+    for arg in get_args() {
+        if expression_opt.is_none() {
+            expression_opt = Some(arg);
+        } else {
+            println!("Error: too many arguments");
+            return;
+        }
+    }
+
+    let expression = match expression_opt {
+        Some(expression) => expression,
+        None => {
+            println!("Enter an expression:");
+            let mut expression = String::new();
+            io::stdin().read_line(&mut expression).unwrap();
+            expression
+        }
+    };
+
+    let mut parser = Parser::new(expression);
     parser.parse();
 
-    // print result
-    println!("{:?}", parser.analyzer.stack);
-    println!("{:?}", parser.analyzer.errors);
+    println!("Stack: {:?}", parser.analyzer.stack);
+    println!("Errors: {:?}", parser.analyzer.errors);
 }
